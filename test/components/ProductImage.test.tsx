ab2d7fa446a83ae5756a33c8f967a8426d50f6e8
Gustavo Hernandez <ghdzplus@gmail.com>
2024-02-26 15:53:00 -0600

    Version 0.0.1 ready

---
+++ test/components/ProductImage.test.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { ProductImage, ProductCard } from '../../src/components';
+import { product2 } from '../data/products';
+
+describe('ProductImage', () => { 
+    
+    test('Debe de mostrar el componente correctamente con la imagen personalizada', () => { 
+        
+        const wrapper = renderer.create(
+           <ProductImage img="https://imagen.jpg" className="custom-class"/>
+        );
+   
+       expect( wrapper.toJSON() ).toMatchSnapshot();
+   });
+
+    test('Debe de mostrar el componente correctamente con la imagen del producto', () => { 
+        
+        const wrapper = renderer.create(
+            <ProductCard product={ product2 }>
+            {
+                () => (
+                    <ProductImage />
+                )
+            }
+            </ProductCard>
+        );
+   
+       expect( wrapper.toJSON() ).toMatchSnapshot();
+   });
+
+});
